3602aaa7c0bc5490061a3ba603518efa3170034d
BAHAAGAMER HD <bahaafayad2006@gmail.com>
2026-01-21 16:48:23 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+import { createClient } from 'npm:@supabase/supabase-js@2'
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://xyzcompany.supabase.co', 'publishable-or-anon-key')
+
+let submitBtn = document.querySelector("#submit-btn")
+if (submitBtn != null) {
+    submitBtn.addEventListener("click", () => {
+        let emailEl = document.querySelector("#input-email")
+        console.log(emailEl.innerHTML)
+        let pasaswordEl = document.querySelector("#input-password")
+        let emailErrorParagraf = document.querySelector(".authentication-format-error-email")
+
+
+
+        let authenticationFeedbackEl = document.querySelector(".authentication-feedback-message")
+        if(!emailEl.value.toLowerCase().includes("@student.mdu.se")){
+            emailErrorParagraf.innerHTML = "Email is incorrectly formatted!"
+            
+        }
+        else{
+            emailErrorParagraf.innerHTML = ""
+            authenticationFeedbackEl.innerHTML = "Trying to Sign up, please wait..."
+        }
+
+    })
+
+}
+
+
+
